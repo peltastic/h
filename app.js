ce6406293f93d5c0815b8744a729c6a1b9a73057
peltastic <onosogapelumi@gmail.com>
2022-10-27 15:07:57 +0100

    make it better

---
+++ app.js
@@ -0,0 +1,18 @@
+const express = require("express")
+const dotenv = require('dotenv')
+dotenv.config()
+const app = express()
+app.use(express.json())
+const PORT = process.env.PORT || 3000
+
+app.get("/", (req, res) => (
+    res.json({
+        slackUsername: "BinsKuka",
+        backend: true,
+        age: 21,
+        bio: "Backend Engineering"
+    })
+))
+app.listen(PORT, () => {
+    console.log(`listening at port ${PORT}`)
+})
