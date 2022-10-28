38a63265be9dec88c2a43ff5e550effd36b1f900
peltastic <onosogapelumi@gmail.com>
2022-10-28 08:45:44 +0100

    hmmm

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
 const express = require("express")
 const dotenv = require('dotenv')
+const cors = require("cors")
 dotenv.config()
 const app = express()
+app.use(cors())
 app.use(express.json())
 const PORT = process.env.PORT || 3000
 
